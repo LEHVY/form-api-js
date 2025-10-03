07970cc0117a86b47ff7ddfce7541747eb5c07d9
LEHVY <amaugolevi@gmail.com>
2025-10-03 23:58:21 +0100

    after completing get method

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-let firstName = document.getElementById("firstname");
-let lastName = document.getElementById("lastname");
-let email = document.getElementById("email");
-let submit = document.getElementById("submitButton");
+const firstName = document.getElementById("firstname");
+const lastName = document.getElementById("lastname");
+const email = document.getElementById("email");
+const submit = document.getElementById("submitButton");
 
 submit.style.color = "red";
 
+// POST METHOD
 submit.addEventListener("click", async (event) => {
     event.preventDefault();
 
-    let url = "https://usersapi.osinachi.me/users";
+    const url = "https://usersapi.osinachi.me/users";
 
     try {
         let res = await fetch(url, {
@@ -17,17 +18,49 @@ submit.addEventListener("click", async (event) => {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                firstName: firstName.value,
-                lastName: lastName.value,
+                first_name: firstName.value,
+                last_name: lastName.value,
                 email: email.value
             })
         });
 
         let data = await res.json();
-        alert("Uploaded");
-        console.log(data)
+        alert("User uploaded successfully!");
+        console.log(data);
     } catch (error) {
-        console.error("Error:",error);
-        alert("Error");
+        console.error("Error:", error);
+        alert("Error uploading user");
     }
 });
+
+// GET METHOD
+const getButton = document.getElementById("getButton");
+const listAll = document.getElementById("listAll");
+
+getButton.addEventListener("click", displayList);
+
+async function displayList(event) {
+    event.preventDefault();
+    const url = "https://usersapi.osinachi.me/users";
+
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        listAll.innerHTML = "";
+
+        const ul = document.createElement("ul");
+
+        data.forEach(user => {
+            const li = document.createElement("li");
+            li.textContent = `firstname: ${user.first_name}, lastname: ${user.last_name}, email: ${user.email}`;
+            
+            ul.appendChild(li);
+        });
+
+        listAll.appendChild(ul);
+
+    } catch (error) {
+        console.error("Error:", error);
+        listAll.innerHTML = "Error getting users";
+    }
+}
